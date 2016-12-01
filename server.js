ebad3789f9e061c834c9b91c2659b377d825e7f6
Ricardo L Pansonato <ricardolp.bsi@gmail.com>
2016-12-01 06:51:16 -0200

    .

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-var serve = serveStatic("./");
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-server.listen(80);
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
